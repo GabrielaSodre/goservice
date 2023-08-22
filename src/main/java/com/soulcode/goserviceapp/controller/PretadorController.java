b655f6ab207bfc2e1365a5890c000db144e182c4
GabrielaSodre <gabi.silvasodre@hotmail.com>
2023-08-22 09:12:18 -0300

    atualizações

---
+++ src/main/java/com/soulcode/goserviceapp/controller/PretadorController.java
@@ -0,0 +1,14 @@
+package com.soulcode.goserviceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/prestador")
+public class PretadorController {
+
+    @GetMapping(value = "/daods")
+    public String dados
+        return dadosPrestador
+}
